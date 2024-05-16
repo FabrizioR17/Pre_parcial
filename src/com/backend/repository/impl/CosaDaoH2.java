c909ab6bd1764fc94735ff157a88f1fe15b61456
FabrizioR17 <fabriziora12@gmail.com>
2024-05-16 09:24:02 -0500

    primer commit

---
+++ src/com/backend/repository/impl/CosaDaoH2.java
@@ -0,0 +1,10 @@
+package com.backend.repository.impl;
+
+import com.backend.entity.Cosa;
+import com.backend.repository.IDao;
+import com.backend.repository.dbconnection.H2Connection;
+import org.apache.log4j.Logger;
+
+import java.sql.*;
+public class CosaDaoH2 {
+}
